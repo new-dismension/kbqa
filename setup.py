7438c32055eb16355ba67913f4f07af17178b9d1
zmz <honomaki@163.com>
2017-02-26 20:56:17 +0800

    init

--- setup.py
+++ setup.py
@@ -1,2 +1,11 @@
+import os
+from setuptools import setup
 
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+setup(
+    name = "kbqa",
+    version = "0.0",
+)
