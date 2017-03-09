271717e3d827da309e43e3aec65dcd9e0990c84e
zmz <honomaki@163.com>
2017-03-09 10:48:37 +0800

    wikidata aliases

--- tests/script/wikidata_to_dict.py
+++ tests/script/wikidata_to_dict.py
@@ -24,6 +24,7 @@ with open('item_dictronary.txt', 'w') as f:
             break
 
         labels = entity['labels']
+        aliases = entity['aliases']
 
         name = None
 
@@ -32,11 +33,14 @@ with open('item_dictronary.txt', 'w') as f:
         elif 'zh-hans' in labels:
             name = labels['zh-hans']['value']
 
+        aliases_zh = None
+        if 'zh' in aliases:
+            aliases_zh = aliases['zh']
+            for zh_aliase in aliases_zh:
+                aliase_zh_value = zh_aliase['value']
+                f.write(aliase_zh_value.encode('utf-8') + '\n')
+
         if name:
             if count % 1000 == 0:
                 print(count, name)
             f.write(name.encode('utf-8') + '\n')
-
-
-
-
